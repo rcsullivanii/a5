2e9bf50d38c506e14e533aec735ea89dbcad56ab
Robert Sullivan <rsullivan@unc.edu>
2025-03-20 13:23:07 -0400

    Implement sleeping barber in elixir

--- README.md
+++ README.md
@@ -4,7 +4,23 @@
 Robert Sullivan and Cole Vita
 
 ## Design Rationale
-Write a README file giving your design rationale. What decisions did you make to get it running? Explain your overall design.
+Our implementation of the Sleeping Barber problem uses concurrent processes to model the Customer, Barber, Waiting Room, Receptionist.
+
+The Receptionist contains most of the logic of the store and recursively updates its state. The state is a containing the waiting room process, barber pid for communication, and whether or not the barber is ready for new customers.
+
+Notably, our waiting room process ensures that a maximum of 6 customers are waiting at any time. Otherwise, the customer leaves. This is done through WaitingRoom's function WaitingRoom.add.
+
+When "popping" a customer from the waiting room, we extract the first customer and update the customers still waiting to be the rest of the list.
+
+The haircut logic can be found in handle_next_customer. handle_next_customer calls WaitingRoom.remove which returns either {:empty} or {:removed, first} where first represents the popped customer. Then handle_next_customer logs that the customer has been seated by the barber and instructs the barber to cut the hair, a randomly timed process with a uniform distribution ~6 seconds.
+
+Anytime either the (1) barber finishes a haircut or (2) a new customer enters and the barber is idle, the receptionist calls handle_next_customer which begins the haircut process.
+
+An important aspect of the waiting room design is ensuring that each of the add and remove requests are processed synchronously (or wait for the return message). These are crucial because it ensures that each call is completed before the program moves on (forces the call to be synchronous). Also, as documented above, remove needs to return whether or not there was a customer to pop.
+
+Our design was set up so that when each customer is spawned, it uses the current version of the Customer module code as with the Barber loop code. Thus, if the program is running processes already, their loop continues to use the previous version whereas new processes spawned can use the more recent version (successfully demonstrating hot swapping)
+
+The program will run forever and requires control c then a to abort.
 
 ## Project Description
 In this assignment we will write an Elixir program to solve the Sleeping Barber problem. Find this problem in an earlier class PPT.
@@ -14,8 +30,7 @@ I want to make some aspects of your solution the same for consistency. There are
 - Let's make a process for the waiting room.  
 - Let's add a receptionist process to the basic problem we defined earlier
   - have this receptionist greet each arriving customer
-  - If there is room to wait, the receptionist sends the customer 
-  - to the waiting room; if no room, the customer is turned away.
+  - If there is room to wait, the receptionist sends the customer to the waiting room; if no room, the customer is turned away.
 - Let's make the barber a process.  
   
 This basic structure gives a lot of places that behavior can be parameterized or varied. The barber can cut hair in fixed time, long or short, or in random amounts of time. The barber can work a while and take a break for a while. The barber might get 2 chairs at some point, and handle 2 customer at once (like one has washed hair drying while another gets a cut). Customers can wait "forever" until served, or made to wait some amount of time and leave if not served. The wait room process can control how customers are handled... FIFO queue, priority some way, random, etc. All these different things can easily be written into the behavior of processes if we have the system architected that way.
@@ -33,3 +48,13 @@ Some initial parameters/behaviors:
 - The wait room is a FIFO queue
 - customers who wait stay until they are served
 - the simulation operates "forever" generating new customers
+
+## Sources
+https://hexdocs.pm/elixir/1.12/GenServer.html
+https://samuelmullen.com/articles/elixir-processes-send-and-receive
+https://stackoverflow.com/questions/35735762/whats-the-difference-between-def-and-defp
+https://hexdocs.pm/elixir/processes.html 
+https://hexdocs.pm/elixir/case-cond-and-if.html
+https://elixirforum.com/t/purpose-of-process-sleep-infinity/4014
+https://stackoverflow.com/questions/38778054/how-to-generate-a-random-number-in-elixir
+https://hexdocs.pm/elixir/modules-and-functions.html
---
+++ sleeping_barber.ex
@@ -0,0 +1,165 @@
+defmodule WaitingRoom do
+  # initializes the waiting room (empty)
+  def start do
+    spawn(fn -> loop([]) end)
+  end
+
+  def loop(customers) do
+    receive do
+      # if receive a request to add to wait list, check if wait is full
+      {:add, customer, reply_to} -> # adding reply_to to add call here for synchronous behavior
+        if length(customers) < 6 do
+          IO.puts("#{customer} enters the waiting room")
+          new_customers = customers ++ [customer] # if not, update customers 
+          send(reply_to, {:added})
+          loop(new_customers) # updates state to include added customer
+        else
+          IO.puts("#{customer} leaves, no space in waiting room.")
+          send(reply_to, {:full})
+          loop(customers) # no update to list, continue
+        end
+
+      # queue behavior here
+      {:remove, reply_to} ->
+        case customers do
+          [first | rest] -> # extract first customer from list (customer is a string here)
+            send(reply_to, {:removed, first}) # both reply_to are for synchronous behavior
+            loop(rest) # updates "state" to exclude the removed customer
+          [] ->
+            send(reply_to, {:empty})
+            loop(customers)
+        end
+    end
+  end
+
+  # add a customer by interacting with WaitingRoom loop
+  # this will be called outside of module so needs to be public
+  def add(room_pid, customer) do
+    send(room_pid, {:add, customer, self()})
+    receive do
+      msg -> msg # callback here ensures synchronous behavior
+    after 5000 ->
+      {:error, :timeout} # error tracing
+    end
+  end
+
+  # removes customer by interacting with WaitingRoom loop 
+  def remove(room_pid) do
+    send(room_pid, {:remove, self()})
+    receive do
+      msg -> msg # callback here ensures synchronous behavior and returns either successully removed (:removed) or not (:empty)
+    after 5000 ->
+      {:error, :timeout} # error tracing
+    end
+  end
+end
+
+defmodule Receptionist do
+  # receptionist represents the main loop for the barbershop (and overall logic for controlling customer flow)
+  # here, state is a map which includes the waiting room process, whether or not barber is busy, and the barber's process id
+  # as follows in the rest of the loops, we use recursion to update the state!
+  def start do
+    # initialize waiting room here
+    waiting_room = WaitingRoom.start()
+    spawn(fn -> loop(%{barber: :idle, barber_pid: nil, waiting_room: waiting_room}) end)
+  end
+
+  def loop(state) do
+    receive do
+      {:customer_arrives, customer} ->
+        # call waiting room api to see if we can add customer 
+        WaitingRoom.add(state.waiting_room, customer) # synchronous call ensures that waiting room is fully processed before next line is processed
+        state = if state.barber == :idle do
+          handle_next_customer(state) # if barber is free, begin haircut process
+        else
+          state
+        end
+        loop(state)
+
+      # update barber state to idle as haircut is done
+      {:barber_done} ->
+        state = %{state | barber: :idle}
+        state = handle_next_customer(state)
+        loop(state)
+
+      # first step of registering the barber pid for all communication
+      {:register_barber, barber_pid} ->
+        state = %{state | barber_pid: barber_pid}
+        loop(state)
+    end
+  end
+
+  # begins haircut process
+  def handle_next_customer(%{waiting_room: room_pid, barber_pid: barber_pid} = state) do
+    # use waiting room api to remove customer (queue logic handled there)
+    case WaitingRoom.remove(room_pid) do
+      {:removed, customer} ->
+        IO.puts("#{customer} goes to the barber")
+        send(barber_pid, {:cut_hair, customer})
+        %{state | barber: :busy}
+      {:empty} ->
+        state
+      _ ->
+        state # for any extraneous messages received
+    end
+  end
+
+  def handle_next_customer(state), do: state
+  def handle_barber_done(state), do: state
+end
+
+defmodule Barber do
+  # starts the barber process and registers its PID with the shop.
+  def start(shop) do
+    pid = spawn(fn -> loop(shop) end)
+    send(shop, {:register_barber, pid})
+    pid
+  end
+
+  def loop(shop) do
+    receive do
+      # cut hair for random amount of time (~6 seconds, uniform distribution)
+      {:cut_hair, customer} ->
+        IO.puts("Cutting hair for #{customer}")
+        :timer.sleep(:rand.uniform(6000))
+        IO.puts("#{customer} is done with the haircut")
+        send(shop, {:barber_done})
+        loop(shop)
+    end
+  end
+end
+
+defmodule Customer do
+  # each customer is their own unique process
+  # for documenting, maintain string with customer id (not pid) we generate and increment
+  def start(shop, id) do
+    spawn(fn -> enter_shop(shop, "Customer #{id}") end)
+  end
+  
+  # notify receptionist of customer's arrival 
+  def enter_shop(shop, customer) do
+    send(shop, {:customer_arrives, customer})
+  end
+end
+
+defmodule CustomerSpawner do
+  def start(shop) do
+    spawn(fn -> loop(shop, 1) end)
+  end
+
+  def loop(shop, id) do
+    # generate new customer randomly (~ 4 seconds, uniform distribution)
+    :timer.sleep(:rand.uniform(4000))
+    Customer.start(shop, id)
+    loop(shop, id + 1)
+  end
+end
+
+# begin
+shop = Receptionist.start()
+Barber.start(shop)
+CustomerSpawner.start(shop)
+
+# sleeping barber will run infinity
+# to abort, press control + c then a
+Process.sleep(:infinity)
